2b3f6a4095905d50a2789bbac7e0e12a93819605
Nama Anda <radityapfarma15@gmail.com>
2025-12-19 02:51:10 +0700

    pers

--- config/database.js
+++ config/database.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI); 
+    console.log("DEBUG MONGO_URI =", process.env.MONGO_URI);
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-    
-    // Handle connection events
+
     mongoose.connection.on('error', (err) => {
       console.error('MongoDB connection error:', err);
     });
@@ -19,9 +20,6 @@ const connectDB = async () => {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
-
-  console.log("DEBUG MONGO_URI =", process.env.MONGO_URI);
-
 };
 
 module.exports = connectDB;
